7a25ddf797f9c386655ed91d336a0d1376ad54a6
Megat Syafiq <syafiqq95@gmail.com>
2019-07-13 15:07:47 +0800

    Initial Commit

---
+++ CountryList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CountryList
+//
+//  Created by Megat Syafiq on 13/07/2019.
+//  Copyright © 2019 Megat Syafiq. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
