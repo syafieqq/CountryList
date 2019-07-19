53f38dc51eb35bb7a6f7f029aff775c8fcfbeef3
Megat Syafiq <syafiqq95@gmail.com>
2019-07-19 12:11:55 +0800

    first commit

---
+++ CountryList/CountryCell.swift
@@ -0,0 +1,24 @@
+//
+//  CountryCell.swift
+//  CountryList
+//
+//  Created by Megat Syafiq on 13/07/2019.
+//  Copyright © 2019 Megat Syafiq. All rights reserved.
+//
+
+import UIKit
+
+class CountryCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ CountryList/Model.swift
@@ -0,0 +1,9 @@
+//
+//  Model.swift
+//  CountryList
+//
+//  Created by Megat Syafiq on 13/07/2019.
+//  Copyright © 2019 Megat Syafiq. All rights reserved.
+//
+
+import Foundation
